7ea5518e14977aaa8877261f48953405abaa9991
Tyo Drak <lptd99@gmail.com>
2024-07-16 12:49:38 -0400

    include auth

--- src/app/firebase/firebaseConfig.tsx
+++ src/app/firebase/firebaseConfig.tsx
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,3 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+export { auth, db };
